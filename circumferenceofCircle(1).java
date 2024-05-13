ac88090607c3d4ede0529cd829dd6029b0e0f992
Isha752 <140087638+Isha752@users.noreply.github.com>
2024-05-13 19:37:46 +0530

    Create circumferenceofCircle(1).java

---
+++ circumferenceofCircle(1).java
@@ -0,0 +1,6 @@
+public class ps1 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+      int r = sc.nextInt();
+        Double C = 2 * Math.PI * r;
+        System.out.println("The circumference of the circle is: " + C);
