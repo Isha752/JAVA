ec350304ebf2c48ea17ae2f6cca70e9da1f8fed7
Isha752 <140087638+Isha752@users.noreply.github.com>
2024-05-13 19:46:42 +0530

    Create stringReplace.java

---
+++ stringReplace.java
@@ -0,0 +1,8 @@
+public class strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      String input = sc.nextLine();
+    String result = input.replace("e", "i");
+    System.out.println(result);
+    }
+}
