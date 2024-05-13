b09ab70477eb28aa5e3b171ddc65e1f0b2e3343e
Isha752 <140087638+Isha752@users.noreply.github.com>
2024-05-13 19:38:58 +0530

    Create CircumferenceofCircle(2).java

---
+++ CircumferenceofCircle(2).java
@@ -0,0 +1,6 @@
+public class ps1 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+      float pie = 3.14f;
+      double C = 2*pie*r;     //Here instead of 'pie' we can also use 'MATH.PI' which is an in-built functionr
+  System.out.println("The circumference of the circle is: " + C);
